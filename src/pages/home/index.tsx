a5fa2a5185abb1df39e3b222b60cd3c72d381a4e
Tan Jun Wei <junwei2509@gmail.com>
2024-06-14 08:58:42 +0800

    Added skeleton loading components

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,36 @@
+import { DealsChart, UpcomingEvents } from "@/components"
+import { Col, Row } from "antd"
+
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Row 
+        gutter={[32,32]}
+        style={{
+          marginTop: '32px'
+        }}
+      >
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: '460ppx'
+          }}
+        >
+          <UpcomingEvents/>
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          xl={16}
+          style={{
+            height: '460ppx'
+          }}
+        >
+          <DealsChart/>
+        </Col>
+      </Row>
+    </div>
   )
 }
